5da7f0026def541fae4725fd4ae40a9043732f9c
olgavvvv <166092988+olgavvvv@users.noreply.github.com>
2026-06-10 15:50:19 +0700

    Create MainWindowViewModel.cs

---
+++ MainWindowViewModel.cs
@@ -0,0 +1,64 @@
+public partial class MainWindowViewModel : ViewModelBase
+{
+    private string _fio = string.Empty;
+    private string _result = string.Empty;
+
+    public string FIO
+    {
+        get => _fio;
+        set => SetProperty(ref _fio, value);
+    }
+    
+    public string Result
+    {
+        get => _result;
+        set => SetProperty(ref _result, value);
+    }
+
+    public async Task GetFio()
+    {
+        var client = new HttpClient();
+        var response = await client.GetAsync("http://89.125.39.39:8080/TransferSimulator/fullName");
+        var content = await response.Content.ReadFromJsonAsync<ResultFio>();
+
+        FIO = content.Value;
+
+        if (Regex.IsMatch(FIO, @"^[А-Яа-яЁ-ё\s]+$"))
+        {
+            Result = "ФИО не содержит запрещенные символы";
+        }
+        else
+        {
+            Result = "ФИО содержит запрещенные символы";
+        }
+    }
+    
+    public void Validation()
+    {
+        const string path = "ТестКейс.docx";
+
+        using var doc = WordprocessingDocument.Open(path, true);
+        var textElements = doc.MainDocumentPart.Document.Descendants<Text>();
+
+        foreach (var textElement in textElements)
+        {
+            if (textElement.Text.Contains("FIO"))
+            {
+                textElement.Text = textElement.Text.Replace("FIO", FIO);
+            }
+            
+            if (textElement.Text.Contains("Res"))
+            {
+                textElement.Text = textElement.Text.Replace("Res", Result);
+            }
+        }
+        
+        doc.MainDocumentPart.Document.Save();
+    }
+}
+
+public class ResultFio
+{
+    public string Value { get; set; }
+}
+
